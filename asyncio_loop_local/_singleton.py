b104007ca02f519dd0cf0d316deabf537fe5f6a0
Alexander Sosedkin <monk@unboiled.info>
2023-10-17 18:45:27 +0200

    singleton: simplify decorating a bit

--- asyncio_loop_local/_singleton.py
+++ asyncio_loop_local/_singleton.py
@@ -44,35 +44,33 @@ def singleton(callable_: typing.Callable[_P, _T]) -> typing.Callable[_P, _T]:
     ...  # overload
 
 
+# I want to be able to later extend it with parameters, so, two forms
 def singleton(
     callable_: typing.Callable[_P, _T] | None = None,
 ) -> typing.Callable[_P, _T] | _Decorator[_P, _T]:
     if callable_ is None:
-        return _singleton()
-    return singleton()(callable_)
+        return _singletonize
+    return _singletonize(callable_)
 
 
-def _singleton() -> _Decorator[_P, _T]:
-    def decorator(f: typing.Callable[_P, _T]) -> typing.Callable[_P, _T]:
-        @functools.wraps(f)
-        def reuse_sync(*args: _P.args, **kwargs: _P.kwargs) -> _T:
-            # sync version wrapping a regular function, no locking required
-            ls = asyncio_loop_local._storage.storage()  # noqa: SLF001
-            try:
-                sc = ls[singleton_cache_key_sentinel]
-            except KeyError:
-                sc = ls[singleton_cache_key_sentinel] = SingletonCache()
-            key = f, args, tuple(kwargs.items())
-            try:
-                return typing.cast(_T, sc[key])
-            except KeyError:
-                pass
-            res = sc[key] = f(*args, **kwargs)
-            return res
+def _singletonize(f: typing.Callable[_P, _T]) -> typing.Callable[_P, _T]:
+    @functools.wraps(f)
+    def reuse_sync(*args: _P.args, **kwargs: _P.kwargs) -> _T:
+        # sync version wrapping a regular function, no locking required
+        ls = asyncio_loop_local._storage.storage()  # noqa: SLF001
+        try:
+            sc = ls[singleton_cache_key_sentinel]
+        except KeyError:
+            sc = ls[singleton_cache_key_sentinel] = SingletonCache()
+        key = f, args, tuple(kwargs.items())
+        try:
+            return typing.cast(_T, sc[key])
+        except KeyError:
+            pass
+        res = sc[key] = f(*args, **kwargs)
+        return res
 
-        return reuse_sync
-
-    return decorator
+    return reuse_sync
 
 
 __all__ = ['singleton']
